e51751f25f6f9e6ef32360b5dc1ae5f12af8b932
Paul M. Lambert <plambert@plambert.net>
2014-12-07 19:02:50 -0800

    kinda working

--- textmate/snippets.sh
+++ textmate/snippets.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if type tm_snippet_xml.pl > /dev/null; then
+if type tm_snippet_xml.pl > /dev/null 2>&1; then
   TM=tm_snippet_xml.pl
 elif [[ -x tm_snippet_xml.pl ]]; then
   TM=./tm_snippet_xml.pl
+elif [[ -x "textmate/tm_snippet_xml.pl" ]]; then
+  TM=textmate/tm_snippet_xml.pl
 else
   echo "$0: tm_snippet_xml.pl: not found in PATH or \"$(pwd)\""
   exit 1
@@ -24,15 +26,309 @@ $TM fieldset \
 
 checkbox )
 
-$TM input --type=checkbox --token:token_name -depends:list,of,required,tokens -rejects:list,of,conflicting,tokens -searchWhenChanged:False change condition default=default_value delimiter earliest latest fieldForLabel fieldForValue label prefix search suffix valuePrefix valueSuffix
+$TM input --type=checkbox \
+  --token:token_name \
+  -id:input_id \
+  -depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  -searchWhenChanged:False \
+  change \
+  condition \
+  default=default_value \
+  delimiter \
+  earliest \
+  latest \
+  fieldForLabel \
+  fieldForValue \
+  label \
+  prefix \
+  search \
+  suffix \
+  valuePrefix \
+  valueSuffix
 
 ;;
 
 dropdown )
 
-$TM input --type=dropdown --token:token_name -depends:list,of,required,tokens -rejects:list,of,conflicting,tokens -searchWhenChanged:False change condition default=default_value allowCustomValues choice delimiter earliest latest fieldForLabel fieldForValue label prefix search suffix valuePrefix valueSuffix selectFirstChoice
+$TM input --type=dropdown \
+  --token:token_name \
+  -id:input_id \
+  -depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  -searchWhenChanged:False \
+  change \
+  condition \
+  default=default_value \
+  allowCustomValues \
+  choice \
+  delimiter \
+  earliest \
+  latest \
+  fieldForLabel \
+  fieldForValue \
+  label \
+  prefix \
+  search \
+  suffix \
+  valuePrefix \
+  valueSuffix \
+  selectFirstChoice
 
 ;;
+
+multiselect )
+
+$TM input --type=multiselect \
+  --token:token_name \
+  -id:input_id \
+  -depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  -searchWhenChanged:False \
+  allowCustomValues \
+  default=default_value \
+  delimiter=" " \
+  earliest \
+  latest \
+  fieldForLabel \
+  fieldForValue \
+  label=label_text \
+  prefix \
+  search \
+  suffix \
+  valuePrefix \
+  valueSuffix
+
+;;
+
+radio )
+
+$TM input --type=radio \
+  --token:token_name \
+  -id:input_id \
+  -depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  -searchWhenChanged:False \
+  change \
+  choice \
+  condition \
+  default=default_value \
+  delimiter=" " \
+  earliest \
+  latest \
+  fieldForLabel \
+  fieldForValue \
+  label=label_text \
+  prefix \
+  search \
+  selectFirstChoice \
+  suffix
+
+;;
+
+text )
+
+$TM input --type=text \
+  --token:token_name \
+  -id:input_id \
+  -depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  -searchWhenChanged:False \
+  change \
+  condition \
+  default=default_value \
+  label=label_text \
+  prefix \
+  seed \
+  suffix
+
+;;
+
+time )
+
+$TM input --type=time \
+  --token:token_name \
+  -id:input_id \
+  -depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  -searchWhenChanged:False \
+  change \
+  condition \
+  default=default_value \
+  earliest \
+  latest \
+  label=label_text
+
+;;
+
+change )
+
+$TM change \
+  condition \
+  set
+
+;;
+
+condition )
+
+$TM condition \
+  --label:\* \
+  --value:\* \
+  -field:\* \
+  link \
+  set \
+  unset
+
+;;
+
+chart )
+
+$TM chart \
+  --id:chart_id \
+  -depends:depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  title \
+  search \
+  drilldown \
+  selection \
+  option \
+  .charting.chart::area,bar,clumn,fillerGauge,line,markerGauge,pie,radialGauge,scatter
+
+;;
+
+event )
+
+$TM event \
+  --id:event_id \
+  -depends:depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  title \
+  search \
+  fields \
+  option \
+  .count:10\? \
+  .displayRowNumbers::False,True \
+  .drilldown::all,none
+
+;;
+
+html )
+
+$TM html \
+  --id:html_id \
+  --tokens:True \
+  -src:file.html \
+  -depends:depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens
+
+;;
+
+map )
+
+$TM map \
+  --id:map_id \
+  -depends:depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  option \
+  title \
+  search
+
+;;
+
+single )
+
+$TM single \
+  --id:single_id \
+  -depends:depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  title \
+  search \
+  option \
+  .afterLabel\? \
+  .beforeLabel\? \
+  .additionalClass:css_class\? \
+  .classField::severe,high,elevated,guarded,low,None\? \
+  .drilldown::none,all\? \
+  .field\? \
+  .underLabel\?
+
+;;
+
+table )
+
+$TM table \
+  --id:table_id \
+  -depends:depends:list,of,required,tokens \
+  -rejects:list,of,conflicting,tokens \
+  title \
+  search \
+  fields \
+  drilldown \
+  format \
+  option \
+  .wrap::True,False \
+  .showPager::True,False \
+  .rowNumbers::False,True \
+  .previewResults::True,False
+
+;;
+
+title )
+
+$TM title
+
+;;
+
+format )
+
+$TM format \
+  --type=sparkline \
+  --field:fieldname \
+  .height:auto\? \
+  .type::line,bar,discrete \
+  option
+
+;;
+
+fields )
+
+$TM fields
+
+;;
+
+option )
+
+$TM option \
+  --name \
+  -1
+
+;;
+
+search )
+
+$TM search \
+  --id:search_id \
+  -base:base_id \
+  -ref:ref_id \
+  -app:app_id \
+  query \
+  earliest \
+  latest
+
+;;
+
+"" )
+
+echo "$0: must give a tag as an argument:"
+perl -ne 'push @t, $1 if (/^(\w+)\s+\)\s*$/); END{print join(", ", sort @t), "\n"}' "$0" 
+
+;;
+
+--all )
+
+perl -ne 'print "$1\n" if (/^(\w+)\s+\)\s*$/);' "$0" | while read tag; do echo "=== $tag ==="; echo; "$0" "$tag"; echo; done
+
+;; 
 
 * )
 
--- textmate/tm_snippet_xml.pl
+++ textmate/tm_snippet_xml.pl
@@ -1,5 +1,29 @@
 #!/usr/bin/perl -w
 
+my $USAGE=<<USAGE;
+# ugly script to produce quick xml snippets for splunk xml items
+
+# tm_snippet_xml.pl <tagname> [options...]
+
+# tagname is the name of the tag itself
+
+# the options are weird; the intention is to be compact, not to be clear.  clear would be to write out the snippets.  :)
+
+# option                      explanation
+# ----------                  ---------------------------------------------------------------------------------------------------
+#  -foo                       add an optional attribute "foo" with a default value of "..."
+#  -foo:bar                   add an optional attribute "foo" with a default value of "bar"
+#  -foo=bar                   add an optional attribute "foo" with a fixed (not edited) value of "bar"
+# --foo                       add a mandatory attribute "foo" with a default value of "..."
+# --foo:bar                   add a mandatory attribute "foo" with a default value of "bar"
+# --foo=bar                   add a mandatory attribute "foo" with a fixed (not edited) value of "bar"
+#   foo                       list "foo" as a next step, do not include an actual "foo" tag
+#   foo=bar                   list "foo" as a next step, include a simple "foo" node with a default value of "bar"
+#  +foo                       do not list "foo" as a next step; this is not very useful
+#  +foo=bar                   do not list "foo" as a next step, but do include a simple "foo" node with a default value of "bar"
+#  -1                         leave result on one line
+USAGE
+
 use strict;
 use warnings;
 
@@ -9,10 +33,53 @@ my @reqattr;
 my @children;
 my $count=1;
 my $attr;
+my $builddir;
+my $snippetname=$tag;
+my $oneline=1;
+
+unless (defined($tag)) {
+  $USAGE =~ s{^# }{}mg;
+  print STDERR $USAGE;
+  print STDERR "\n$0: error: no tag given\n";
+  exit 1;
+}
+
+if (-d "build") {
+  $builddir="build";
+}
+elsif (-d "../build") {
+  $builddir="../build";
+}
+elsif (-d "../../build") {
+  $builddir="../../build";
+}
+else {
+  die "$0: build: cannot find build directory ./build, ../build, or ../../build\n";
+}
+
+if (grep { m{^--type=} } @ARGV) {
+  for (@ARGV) {
+    if (m{^--type=(.*)}) {
+      $snippetname="${tag}-$1";
+      last;
+    }
+  }
+}
+
+unless (open(SNIPPET, ">", "${builddir}/${snippetname}.snippet")) {
+  die "$0: ${builddir}/${snippetname}.snippet: could not open for writing: $!\n";
+}
+select(SNIPPET);
 
 while(@ARGV) {
   my $opt=shift @ARGV;
-  if ($opt =~ s{^--}{}) {
+  if ($opt eq '-1') {
+    $oneline=1;
+  }
+  elsif ($opt eq '-2') {
+    undef $oneline;
+  }
+  elsif ($opt =~ s{^--}{}) {
     push @reqattr, attr_from_opt($opt);
   }
   elsif ($opt =~ s{^-}{}) {
@@ -33,7 +100,7 @@ sub attr_from_opt {
     ($attr->{name}, $attr->{default}) = ($1, $2);
   }
   else {
-    ($attr->{name}, $attr->{default}) = ($1, '...');
+    ($attr->{name}, $attr->{default}) = ($opt, '...');
   }
   return $attr;
 }
@@ -44,7 +111,14 @@ sub child_from_opt {
   if ($opt =~ s{^\+}{}) {
     $child->{list} = 0;
   }
-  if ($opt =~ m{^(.*?)=(.*)}) {
+  if ($opt =~ m{^\.(.*?)(?:::(.*?))?(?::(.*?))?(\?)?$}) {
+    $child->{opt}=$1;
+    $child->{alt}=$2 if (defined($2));
+    $child->{default}=$3 if (defined($3));
+    $child->{del}=1 if (defined($4));
+    $child->{list}=0;
+  }
+  elsif ($opt =~ m{^(.*?)=(.*)}) {
     $child->{name}=$1;
     $child->{value}=$2;
   }
@@ -79,11 +153,41 @@ sub print_attr {
 sub print_children {
   my $cnt=shift;
   for my $child (@_) {
-    if (defined($child->{value})) {
-      printf "  <%s>\${%d:%s}</%s>\n", $child->{name}, $cnt, $child->{value}, $child->{name};
+    undef $oneline if (defined($child->{opt}) or defined($child->{value}));
+    if (defined($child->{opt})) {
+      $cnt=print_option($cnt, $child);
+    }
+    elsif (defined($child->{value})) {
+      printf "%s<%s>\${%d:%s}</%s>\n", ($oneline ? "" : "  "), $child->{name}, $cnt, $child->{value}, $child->{name};
       $cnt += 1;
     }
   }
+  return $cnt;
+}
+
+# opt, alt, default, del
+sub print_option {
+  my ($cnt, $child) = @_;
+  printf "  " unless ($oneline);
+  if (defined($child->{del}) and $child->{del}) {
+    printf "\$\{%d:", $cnt;
+    $cnt+=1;
+  }
+  printf "<option name=\"%s\">", $child->{opt};
+  if (defined($child->{alt})) {
+    printf "\$\{%d|%s\}", $cnt, $child->{alt};
+    $cnt += 1;
+  }
+  else {
+    $child->{default}='...' unless (defined($child->{default}));
+    printf "\$\{%d:%s\}", $cnt, $child->{default};
+    $cnt += 1;
+  }
+  printf "</option>";
+  if (defined($child->{del}) and $child->{del}) {
+    printf "}";
+  }
+  printf "\n";
   return $cnt;
 }
 
@@ -99,15 +203,22 @@ if (@attr) {
   }
   printf "}";
 }
-printf ">\n";
+printf ">%s", $oneline ? "" : "\n";
 $count=print_children($count, @children);
 
+printf "%s", ($oneline ? "" : "  ");
 if (@children) {
-  printf "  \$\{0:<!-- continue: %s ... -->\}\n", join(", ", sort map { $_->{name} } grep { $_->{list} } @children);
+  printf "\$\{0:<!-- continue: %s ... -->\}", join(", ", sort map { $_->{name} } grep { $_->{list} } @children);
 }
 else {
-  printf "  \$0\n";
+  printf "\$0";
 }
+printf "\n" unless ($oneline);
 
 printf "</%s>\n", $tag;
+
+select(STDOUT);
+close(SNIPPET);
+
+system "cat", "${builddir}/${snippetname}.snippet"
 
